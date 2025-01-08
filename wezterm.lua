17286d3b9c85cbd7f19def622346e3e6a81acef0
Hugo Baba <hugobarette@gmail.com>
2025-01-08 11:25:36 +0100

    Add copy & paste with right click

--- wezterm.lua
+++ wezterm.lua
@@ -1,12 +1,13 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
+local action = wezterm.action
 
 -- Program to lauch when starting Wezterm
 config.default_prog = { "powershell" }
 
 -- Font
 config.font = wezterm.font("JetBrainsMono NFM")
-config.font_size = 16
+config.font_size = 14
 
 -- Theme
 config.color_scheme = "Catppuccin Mocha"
@@ -28,18 +29,39 @@ config.keys = {
   {
     key = 'v',
     mods = 'CTRL|SHIFT|ALT',
-    action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+    action = action.SplitVertical { domain = 'CurrentPaneDomain' },
   },
   {
     key = 'h',
     mods = 'CTRL|SHIFT|ALT',
-    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+    action = action.SplitHorizontal { domain = 'CurrentPaneDomain' },
   },
   {
     key = 'w',
     mods = 'CTRL',
-    action = wezterm.action.CloseCurrentPane { confirm = true },
+    action = action.CloseCurrentPane { confirm = true },
   },
+  {
+    key = '$',
+    mods = 'CTRL',
+    action = action.Hide,
+  },
+}
+
+config.mouse_bindings = {
+	{
+    -- Copy & Paste with mouse's right click
+		event = { Down = { streak = 1, button = "Right" } },
+		mods = "NONE",
+		action = wezterm.action_callback(function(window, pane)
+			local has_selection = window:get_selection_text_for_pane(pane) ~= ""
+			if has_selection then
+				window:perform_action(action.CopyTo("ClipboardAndPrimarySelection"), pane)
+				window:perform_action(action.ClearSelection, pane)
+        window:perform_action(action({ PasteFrom = "Clipboard" }), pane)
+			end
+		end),
+	},
 }
 
 return config
